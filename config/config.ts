65e3aef9b4a60fa37ca4154ab58cf1da76909f84
Damien Vesper <ldamienvesper@gmail.com>
2021-07-03 20:22:33 -0500

    fix build

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,18 @@
+import { author, version } from '../package.json';
+
+import colors from './colors';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
-    prefix: `!`
+    colors,
+
+    developerID: `386940319666667521`,
+    prefix: `!`,
+
+    version,
+    footer: `© Created by ${author} | v${version}`
 };
 
 export default config;
